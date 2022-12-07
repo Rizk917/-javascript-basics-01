fd28b875932da52cd1216034c97629150920c2b1
Rizk917 <117639834+Rizk917@users.noreply.github.com>
2022-12-07 21:20:29 +0200

    step 7

---
+++ step-7/step-7.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    const x = document.getElementById("NUM1").value;
+    const y = document.getElementById("NUM2").value;
+    const z=((((Number(x)*2)+5)*50)-Number(y)+1766);
+
+    alert(z);
+  
+  }
