74d7687a7f6c805be9bd2288eea2359326221814
Rizk917 <117639834+Rizk917@users.noreply.github.com>
2022-12-07 13:39:38 +0200

    step4

---
+++ step-4/step-4.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    var x = document.getElementById("fText").value;
+    var y = document.getElementById("ltext").value;
+    var z = document.getElementById("ctext").value;
+    alert("First Name: "+x+"\n Last Name: "+y+"\n City: "+z);
+  
+  }
